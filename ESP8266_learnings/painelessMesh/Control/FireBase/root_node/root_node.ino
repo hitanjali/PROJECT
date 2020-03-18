b5e234606432dca1fbed7b892d184e57896d1c91
Hitesh Joshi <nu.electronics@zoho.com>
2020-03-18 16:07:20 +0530

    Added firebase code and separated MQTT

---
+++ ESP8266_learnings/painelessMesh/Control/FireBase/root_node/root_node.ino
@@ -0,0 +1,171 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library to
+// connect to a another network and relay messages from a MQTT broker to the nodes of the mesh network.
+// To send a message to a mesh node, you can publish it to "painlessMesh/to/12345678" where 12345678 equals the nodeId.
+// To broadcast a message to all nodes in the mesh you can publish it to "painlessMesh/to/broadcast".
+// When you publish "getNodes" to "painlessMesh/to/gateway" you receive the mesh topology as JSON
+// Every message from the mesh which is send to the gateway node will be published to "painlessMesh/from/12345678" where 12345678 
+// is the nodeId from which the packet was send.
+//************************************************************
+
+#include <Arduino.h>
+#include <painlessMesh.h>
+//#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include "FirebaseESP8266.h"
+
+#define   MESH_PREFIX     "gloria1B"
+#define   MESH_PASSWORD   "gloria1B_pass"
+#define   MESH_PORT       5555
+
+#define   STATION_SSID     "NU_JIO"
+#define   STATION_PASSWORD "j0jioNathan"
+
+#define FIREBASE_HOST "m-test-ba6ba.firebaseio.com"
+#define FIREBASE_AUTH "gJcnVUObNLDDPv89iHQiypu8VPDwsNem0zupxuoj"
+
+#define HOSTNAME "Firebase_host"
+
+const unsigned long TIMER         = 1000; 
+
+// Prototypes
+void receivedCallback( const uint32_t &from, const String &msg );
+
+IPAddress getlocalIP();
+
+IPAddress myIP(0,0,0,0);
+//IPAddress mqttBroker(broker.hivemq.com);
+
+FirebaseData firebaseData;
+painlessMesh  mesh;
+WiFiClient wifiClient;
+uint8_t BuiltIn_LED = 2;
+
+unsigned long start_time       = 0;
+
+unsigned long new_time         = 0;
+
+int fb_start = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+    pinMode(BuiltIn_LED, OUTPUT);
+
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
+  mesh.onReceive(&receivedCallback);
+
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  
+
+  mesh.setRoot(true);
+  // This and all other mesh should ideally now the mesh contains a root
+  mesh.setContainsRoot(true);
+
+  start_time = millis();
+}
+
+void loop() {
+
+  
+  mesh.update();
+
+  
+  new_time = millis();
+  if ((new_time - start_time >= TIMER ) && fb_start) {
+   firebaseGetmsg();
+   start_time = millis();
+  }
+  
+  if(myIP != getlocalIP()){
+    myIP = getlocalIP();
+    Serial.println("My IP is " + myIP.toString());
+
+    fb_start = 1;
+    
+  }
+}
+
+void receivedCallback( const uint32_t &from, const String &msg ) {
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+  String topic = "painlessMesh/from/" + String(from);
+  // mqttClient.publish(topic.c_str(), msg.c_str());
+  // Put the message to the firebase corresponding node
+}
+
+// Get message from the firebase and borascast/send to corresponsing node
+void firebaseGetmsg () {
+ 
+ if(Firebase.get(firebaseData, "/nodes/node1/power"))
+  {
+    //Success
+    Serial.print("Get int data success, int = ");
+    String val = firebaseData.stringData();
+    Serial.println(val);
+
+    if(val == "on") {
+      mesh.sendBroadcast("ON");
+      digitalWrite(BuiltIn_LED, LOW);
+    } else {
+      digitalWrite(BuiltIn_LED, HIGH);
+      mesh.sendBroadcast("OFF");
+    }
+   }else{
+    //Failed?, get the error reason from firebaseData
+
+    Serial.print("Error in getInt, ");
+    Serial.println(firebaseData.errorReason());
+  }
+
+
+}
+
+/*
+void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+  char* cleanPayload = (char*)malloc(length+1);
+  payload[length] = '\0';
+  memcpy(cleanPayload, payload, length+1);
+  String msg = String(cleanPayload);
+  free(cleanPayload);
+
+  String targetStr = String(topic).substring(16);
+
+  if(targetStr == "gateway")
+  {
+    if(msg == "getNodes")
+    {
+      mqttClient.publish("painlessMesh/from/gateway", mesh.subConnectionJson().c_str());
+    }
+  }
+  else if(targetStr == "broadcast") 
+  {
+    mesh.sendBroadcast(msg);
+  }
+  else
+  {
+    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
+    if(mesh.isConnected(target))
+    {
+      mesh.sendSingle(target, msg);
+    }
+    else
+    {
+      mqttClient.publish("painlessMesh/from/gateway", "Client not connected!");
+    }
+  }
+}
+*/
+
+IPAddress getlocalIP() {
+  return IPAddress(mesh.getStationIP());
+}
---
+++ ESP8266_learnings/painelessMesh/Control/MQTT/leaf_node_control/leaf_node_control.ino
@@ -0,0 +1,85 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+//
+// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
+// 2. prints anything it receives to Serial.print
+//
+//
+//************************************************************
+#include "painlessMesh.h"
+
+#define   MESH_PREFIX     "gloria1B"
+#define   MESH_PASSWORD   "gloria1B_pass"
+#define   MESH_PORT       5555
+// #define   LED              2     For ESP 01 - Use GPIO 2
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+String status = "NULL";
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+  String msg = String(status + " : ");
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+
+  if ( msg == "ON")
+  {
+  	digitalWrite(LED_BUILTIN,LOW); //For ESP8266 1=0
+	  status = "ON";
+  }
+  else if (msg == "OFF")
+  {
+  	digitalWrite(LED_BUILTIN,HIGH); //For ESP8266 1=0
+	  status = "OFF";
+  }
+
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN,OUTPUT);
+  Serial.begin(115200);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT , WIFI_AP_STA, 6);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+  // This and all other mesh should ideally now the mesh contains a root
+  mesh.setContainsRoot(true);
+
+}
+
+void loop() {
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
+}
